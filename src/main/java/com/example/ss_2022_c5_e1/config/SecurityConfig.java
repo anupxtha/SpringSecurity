2985ab935fc96ec51df3a13a4dbdaa3b2d2b09f5
AnupXtha <anup.shrestha@citytech.global>
2023-10-30 10:25:46 +0545

    Spring Endpoint authorization

---
+++ src/main/java/com/example/ss_2022_c5_e1/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.example.ss_2022_c5_e1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     return http.httpBasic()
+             .and()
+             // matcher method + authorization rules
+             // 1. Which matcher method should you use and how  ( anyRequest(), mvnMatchers() --> recommended, antMatchers() --> Not Recommended, regexMatchers() )
+             // 2. how to apply different authorization rules
+             .authorizeRequests()
+//             .anyRequest().authenticated()  // endpoint level authorization
+//             .anyRequest().permitAll()
+//             .anyRequest().denyAll()
+//             .anyRequest().hasAnyAuthority("read")
+//             .anyRequest().hasAnyAuthority("read", "write")
+//             .anyRequest().hasRole("ADMIN")
+//             .anyRequest().hasAnyRole("ADMIN","CLIENT")
+//             .anyRequest().access("isAuthenticated() and hasAuthority('read')")  // SpEl ---> authorization rules
+//             .mvcMatchers("/demo").hasAnyRole("admin","client")
+             .mvcMatchers("/demo").hasAnyAuthority("read","ROLE_admin")
+             .anyRequest().authenticated()
+             .and()
+             .build();
+
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var uds = new InMemoryUserDetailsManager();
+        var u1 = User.withUsername("bill")
+                .password(passwordEncoder().encode("12345"))
+                .authorities("read", "ROLE_admin")
+//                .roles("ADMIN")
+                .build();
+
+        var u2 = User.withUsername("john")
+                .password(passwordEncoder().encode("12345"))
+                .authorities("write","ROLE_client")
+//                .roles("CLIENT")
+                .build();
+
+        uds.createUser(u1);
+        uds.createUser(u2);
+
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
